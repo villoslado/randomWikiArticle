c2c6a79ead48a0437aa80477148f0ee80c9d8036
villoslado <jg.villoslado@gmail.com>
2024-08-25 00:11:30 -0700

    Python script completed

---
+++ random_wiki.py
@@ -0,0 +1,31 @@
+import wikipediaapi
+import random
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+def get_random_article_in_category(category_name, language="en"):
+
+    user_agent = os.getenv("USER_AGENT")
+
+    wiki_wiki = wikipediaapi.Wikipedia(
+        user_agent=user_agent,
+        language=language,
+    )
+    category_page = wiki_wiki.page(f"Category:{category_name}")
+    pages = list(category_page.categorymembers.values())
+    articles = [page for page in pages if page.ns == 0]
+
+    if articles:
+        random_page = random.choice(articles)
+        return random_page.title, random_page.summary
+    else:
+        return None, "No articles found in this category"
+
+
+# Example usage for "Software development"
+title, summary = get_random_article_in_category("Software_development")
+print(f"Title: {title}\nSummary: {summary}")
